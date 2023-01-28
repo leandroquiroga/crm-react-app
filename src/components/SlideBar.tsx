00b239be6fad051f9475c63dd315fc161b113ea5
leandroquiroga <leandroquiroga9514@gmail.com>
2023-01-28 17:55:29 -0300

    componnet: Sildebar Page

---
+++ src/components/SlideBar.tsx
@@ -0,0 +1,69 @@
+import { useLocation } from 'react-router-dom';
+import menuBar from '../assets/menu.svg';
+import { Link } from 'react-router-dom';
+
+export const SlideBar = () => {
+
+  const { pathname } = useLocation();
+
+  
+  return (
+    <aside className='flex-initial w-80 bg-sky-900 text-white flex flex-col flex-wrap justify-between'>
+
+      <div className='flex justify-between items-center p-2'>
+        <h1 className='text-xl font-bold'> CRM Client</h1>
+        <img src={menuBar} alt="menu" className='cursor-pointer' id='menu'/>
+      </div>
+
+      <div className='flex justify-center items-center flex-col p-3'>
+          <Link
+          to='/dashboard'
+          className={
+            `${pathname === '/dashboard'
+              ? 'text-gray-400'
+              : 'text-white'
+            } my-3 hover:text-gray-300 transition-all`}
+          >
+            Dashboard
+          </Link>
+          <Link
+            to='/newclient'
+            className={
+              `${pathname === '/newclient'
+                ? 'text-gray-400'
+                : 'text-white'
+              } my-3 hover:text-gray-300 transition-all`}
+          >
+            Cargar Cliente
+          </Link>
+          <Link
+            to='/updateclient'
+            className={
+              `${pathname === '/updateclient'
+                ? 'text-gray-400'
+                : 'text-white'
+              } my-3 hover:text-gray-300 transition-all`}
+          >
+            Editar Cliente
+          </Link>
+          <Link
+            to='/user-perfil'
+            className={
+              `${pathname === '/user-perfil'
+                ? 'text-gray-400'
+                : 'text-white'
+              } my-3 hover:text-gray-300 transition-all`}
+          >
+            Mi Perfil
+          </Link>
+      </div>
+
+      <footer className='p-3 my-3 text-center'>
+          <a href="https://github.com/leandroquiroga" target="_blank" rel="noopener noreferrer">
+            by Leandro Quiroga
+          </a>
+      </footer>
+
+    </aside>
+  )
+}
