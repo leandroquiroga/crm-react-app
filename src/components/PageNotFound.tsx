bc88439069ef66c1032254fa9be8f9903abd35b9
leandroquiroga <leandroquiroga9514@gmail.com>
2023-01-29 19:26:10 -0300

    change: Has been changed UI

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const PageNotFound = () => {
   return (
-    <div className='text-center font-bold text-xxl'> 404 - Page not Found</div>
+    <div> 404 - Page not Found</div>
   )
 }
