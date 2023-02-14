d2a412b9a6768b1043b894c5b34f5441fc51d0a5
leandroquiroga <leandroquiroga9514@gmail.com>
2023-02-14 20:59:11 -0300

    add: Has been added interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,15 @@
+export interface InitialValues {
+  code: number;
+  zone: string;
+  geocent: string;
+  locality: string;
+  province: string;
+}
+
+export interface ErrorValues { 
+  code?: string;
+  zone?: string;
+  geocent?: string;
+  locality?: string;
+  province?: string;
+}
