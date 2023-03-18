dc8c97c322969a6f4cac4ca77d0e0e26c71b4420
leandroquiroga <leandroquiroga9514@gmail.com>
2023-03-18 20:38:31 -0300

    fix: Has been modified prettier in component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,92 +8,65 @@ import { Link } from 'react-router-dom';
 export const Sidebar = () => {
   const { pathname } = useLocation();
   return (
-    <Box
-      className='flex-initial w-96 flex flex-col justify-between p-3 bg-cyan-700'
-    >
-        <Box className='flex justify-between items-center p-2'>
+    <Box className="flex-initial w-96 flex flex-col justify-between p-3 bg-cyan-700">
+      <Box className="flex justify-between items-center p-2">
+        <Typography className="text-xl font-bold text-white" variant="h5">
+          CRM Client
+        </Typography>
 
-          <Typography
-            className='text-xl font-bold text-white'
-            variant='h5'
+        <AccordionSummary
+          expandIcon={<MenuRoundedIcon color={"action"} height={2 + "em"} />}
+          aria-controls="menu"
+          id="menu"
+        ></AccordionSummary>
+      </Box>
+      <Box className="text-center">
+        <List component="nav" aria-label="mailbox folders">
+          <Link
+            to="/"
+            className={`${
+              pathname === "/" ? "text-gray-400" : "text-white"
+            } my-3 hover:text-gray-300 transition-all`}
           >
-            CRM Client
-          </Typography>
-
-          <AccordionSummary
-            expandIcon={
-              < MenuRoundedIcon
-                color={'action'}
-                height={2 + 'em'}
-              />
-              }
-            aria-controls='menu'
-            id='menu'
+            Dashboard
+          </Link>
+          <Divider />
+          <Link
+            to="/newclient"
+            className={`${
+              pathname === "/newclient" ? "text-gray-400" : "text-white"
+            } my-3 hover:text-gray-300 transition-all`}
           >
-          </AccordionSummary>
-          
-        </Box>
-        <Box
-          className= 'text-center'
-        >
+            Cargar Cliente
+          </Link>
+          <Divider />
+          <Link
+            to="/editclient"
+            className={`${
+              pathname === "/updateclient" ? "text-gray-400" : "text-white"
+            } my-3 hover:text-gray-300 transition-all`}
+          >
+            Editar Cliente
+          </Link>
+          <Divider light />
+          <Link
+            to="/user"
+            className={`${
+              pathname === "/user-perfil" ? "text-gray-400" : "text-white"
+            } my-3 hover:text-gray-300 transition-all`}
+          >
+            Mi Perfil
+          </Link>
+        </List>
+      </Box>
 
-          <List component="nav" aria-label="mailbox folders">
-              <Link
-                to='/'
-                className={
-                  `${pathname === '/'
-                    ? 'text-gray-400'
-                    : 'text-white'
-                  } my-3 hover:text-gray-300 transition-all`}
-                >
-                  Dashboard
-              </Link>
-            <Divider />
-              <Link
-                to='/newclient'
-                className={
-                  `${pathname === '/newclient'
-                    ? 'text-gray-400'
-                    : 'text-white'
-                  } my-3 hover:text-gray-300 transition-all`}
-              >
-                Cargar Cliente
-              </Link>
-            <Divider />
-              <Link
-                to='/editclient'
-                className={
-                  `${pathname === '/updateclient'
-                    ? 'text-gray-400'
-                    : 'text-white'
-                  } my-3 hover:text-gray-300 transition-all`}
-              >
-                Editar Cliente
-              </Link>
-            <Divider light />
-              <Link
-                to='/user'
-                className={
-                  `${pathname === '/user-perfil'
-                    ? 'text-gray-400'
-                    : 'text-white'
-                  } my-3 hover:text-gray-300 transition-all`}
-              >
-                Mi Perfil
-              </Link>
-          </List>
-        </Box>
-
-        <Box>
-          <footer className='p-3 my-3 text-center text-white'>
-            <Link
-              to='https://github.com/leandroquiroga'
-              target='_blank'
-            >
-              By Leandro Quiroga
-            </Link>
-          </footer>
-        </Box>
+      <Box>
+        <footer className="p-3 my-3 text-center text-white">
+          <Link to="https://github.com/leandroquiroga" target="_blank">
+            By Leandro Quiroga
+          </Link>
+        </footer>
+      </Box>
     </Box>
   );
 };
